ba1155b12038d1b4be82214e944d8653b5aefccd
Martin Cook <33993749+martycook@users.noreply.github.com>
2023-02-03 14:51:18 -0600

    tweaks

--- src/CG.EntityFrameworkCore/Options/DataAcessLayerOptions.cs
+++ src/CG.EntityFrameworkCore/Options/DataAcessLayerOptions.cs
@@ -29,28 +29,12 @@ public class DataAcessLayerOptions
     /// This property directs the DAL to drop the underlying database on 
     /// startup.
     /// </summary>
-    /// <remarks>
-    /// <para>
-    /// This property is ignored in production.
-    /// </para>
-    /// </remarks>
     public bool DropDatabaseOnStartup { get; set; }
 
     /// <summary>
     /// This property directs the DAL to migrate the underlying database 
     /// on startup.
     /// </summary>
-    /// <remarks>
-    /// <para>
-    /// This property is ignored in production.
-    /// </para>
-    /// <para>
-    /// If the <see cref="DataAcessLayerOptions.DropDatabaseOnStartup"/> property is set to 
-    /// true then migrations are always applied, since the database is 
-    /// then dropped and re-created. This property allows migrations to 
-    /// be applied without dropping and re-creating the database.
-    /// </para>
-    /// </remarks>
     public bool MigrateDatabaseOnStartup { get; set; }
 
     #endregion
